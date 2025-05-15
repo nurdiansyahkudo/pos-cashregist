619e6c0c000c8ea70ce3eb881704a255d411bd5e
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-15 10:30:34 +0700

    UPDATE: whole module

--- static/src/js/openRegister.js
+++ static/src/js/openRegister.js
@@ -1,14 +1,26 @@
-odoo.define('pos_cashregist.openRegister', function (require) {
-    const PaymentScreen = require('point_of_sale.PaymentScreen');
-    const { patch } = require("@web/core/utils/patch");
+import { patch } from 'web.utils';
+import { useListener } from 'web.custom_hooks';
+import { Registries } from '@web/core/registry';
 
-    patch(PaymentScreen.prototype, {
-        async _finalizeValidation() {
-            // Langsung cetak tanpa dialog konfirmasi
-            if (this.env.pos.config.iface_print_via_proxy || this.env.pos.config.iface_print_auto) {
-                this.printReceipt();
+// PosPaymentScreen ada di module point_of_sale. Kita extend method printReceipt
+const PosPaymentScreen = (await import('point_of_sale.PaymentScreen')).PaymentScreen;
+
+patch(PosPaymentScreen.prototype, 'pos_cashdrawer_open', {
+    async printReceipt() {
+        // Panggil method asli printReceipt dulu
+        await this._super(...arguments);
+
+        // Cek apakah metode pembayaran yang terakhir adalah cash
+        const paymentLines = this.currentOrder.paymentLines.models;
+        if (paymentLines.length) {
+            const lastPayment = paymentLines[paymentLines.length - 1];
+            if (lastPayment.payment_method && lastPayment.payment_method.is_cash_count) {
+                // Ini method bawaan POS untuk buka cashdrawer via printer
+                this.env.pos.proxy.printer.open_cashbox();
+                console.log('Cash drawer should be opened');
             }
-            return await this._super(...arguments);
-        },
-    });
+        }
+    }
 });
+
+Registries.Component.extend(PosPaymentScreen, {});
